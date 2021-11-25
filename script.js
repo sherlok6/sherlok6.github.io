fb22f67c1def45e4de46062bf0e4a37338f055eb
sherlok6 <72686118+sherlok6@users.noreply.github.com>
2021-11-26 00:21:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function show(){
+    document.getElementById("window").className = "show"
+    document.getElementById("bt").className = "hide"
+}
+
+function hide(){
+    document.getElementById("window").className = "hide"
+    document.getElementById("bt").className = "bt"
+}
